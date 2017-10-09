6ee4b42bdad35866be51300b2c5a13bb0a601888
jengleberg <jeffengleberg@hotmail.com>
2017-10-09 12:14:43 -0600

    adds one name and add restaruant

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var mongo = require("mongodb").MongoClient;
 var prompt = require("prompt-sync")();
 var url = "mongodb://localhost:27017/restaurant_db";
 
+
+
 mongo.connect(url, function(err, db){
   var collection = db.collection('restaurants');
+  //Prompt to see all restaurants
   var allChoice = prompt("Type 'all' and press enter to display all restaurants' names: ");
-  
   if(allChoice == "all"){
     collection.find().toArray(function(err, doc){
     	console.log(doc);
@@ -13,13 +15,20 @@ mongo.connect(url, function(err, db){
   } else {
    console.log("Aw, you don't want to see the restaurants?");
   	}  
-  	var oneChoice = prompt("Type the name of the restaurant and press enter to display that restaurants' name: ");
-  	if (oneChoice == "one"){
-    	collection.find().toArray(function(err, doc){ 
-  		console.log("Restaurant " + name );
-  		});
-    } else {
-    	console.log("Wrong name");
-    }
 
+    // Promt to see one restaurant
+  	var oneChoice = prompt("Type the name of the restaurant and press enter to display that restaurants' name: ");
+    	collection.find({name: oneChoice }).toArray(function(err, doc){ 
+  		console.log(doc);
+  		});
+
+   var newName = prompt("Enter the name of the new restaurant. name: ");
+   var newStreet = prompt("Enter the street of the new restaurant. street: ");
+   var newZipCode = prompt("Enter the zipcode of the new restaurant. Zipcode: ");
+   var newYelp = prompt("Enter the yelp url of the new restaurant. url: ");
+   collection.insert(
+      {name: newName,
+       address: {street: newStreet, zipcode: newZipCode},
+       yelp: newYelp}
+    );
 });
