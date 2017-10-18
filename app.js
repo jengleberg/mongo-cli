a8f309fde679fdf96e3bdf77931104536a0ef497
jengleberg <jeffengleberg@hotmail.com>
2017-10-18 14:37:52 -0600

    adds update and delete

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var prompt = require("prompt-sync")();
 var url = "mongodb://localhost:27017/restaurant_db";
 
 
-
-mongo.connect(url, function(err, db){
+//Prompt to see all restaurants
+  mongo.connect(url, function(err, db){
   var collection = db.collection('restaurants');
-  //Prompt to see all restaurants
+
   var allChoice = prompt("Type 'all' and press enter to display all restaurants' names: ");
   if(allChoice == "all"){
     collection.find().toArray(function(err, doc){
@@ -15,13 +15,20 @@ mongo.connect(url, function(err, db){
   } else {
    console.log("Aw, you don't want to see the restaurants?");
   	}  
+  });
 
     // Promt to see one restaurant
+     mongo.connect(url, function(err, db){
+   var collection = db.collection('restaurants');
   	var oneChoice = prompt("Type the name of the restaurant and press enter to display that restaurants' name: ");
     	collection.find({name: oneChoice }).toArray(function(err, doc){ 
   		console.log(doc);
   		});
+    }); 
 
+   // promt for adding restaurant
+   mongo.connect(url, function(err, db){
+   var collection = db.collection('restaurants');
    var newName = prompt("Enter the name of the new restaurant. name: ");
    var newStreet = prompt("Enter the street of the new restaurant. street: ");
    var newZipCode = prompt("Enter the zipcode of the new restaurant. Zipcode: ");
@@ -31,4 +38,24 @@ mongo.connect(url, function(err, db){
        address: {street: newStreet, zipcode: newZipCode},
        yelp: newYelp}
     );
+ });
+
+
+// Prompt for editing 
+  mongo.connect(url, function(err, db){
+  var collection = db.collection('restaurants');
+  var updateName = prompt('Enter the name of the restaurant you wish to update. Name: ');
+  var updateTrait = prompt('Enter the name of the field you would like to update or add. Field:');
+  console.log(updateTrait);
+  var updateVal = prompt('Enter the information for the field mentioned above. Info: ');
+  collection.update({name: updateName}, {$set: { updateTrait: updateVal} });
+  console.log(updateTrait);
+});
+
+// Prompt for deleting
+mongo.connect(url, function(err, db) {
+  var collection = db.collection('restaurants');
+  var deleteOne = prompt('Enter the name of the restaurant you wish to delete. Name: ');
+    collection.remove({name: deleteOne });
+    console.log(deleteOne);
 });
